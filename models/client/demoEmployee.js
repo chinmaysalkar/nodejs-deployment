7a0526b9796ae0de65f32ed4fb9842a6c3b72ba8
Nihal Sakharkar <88158315+nihalsakharkar@users.noreply.github.com>
2024-02-16 10:43:53 +0530

    Update demoEmployee.js

--- models/client/demoEmployee.js
+++ models/client/demoEmployee.js
@@ -1,9 +1,1 @@
-const mongoose = require("mongoose");
 
-const employeeSchema = new mongoose.Schema({
- employeeName:{type:String,required:[true, "user name is required"]},
- work:{type:String, required:[true,"employee work is required"]}
-
-})
-const Employee= mongoose.model("Employee",employeeSchema)
-module.exports = Employee
